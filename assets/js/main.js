38a7f5f851089053311f828fb9cc4a0a90abe9ff
RZTGaming <rztprogaming@gmail.com>
2024-12-17 18:58:24 +0800

    upload majalah digital

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$('#tenaga-pendidik-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    items: 3,
+    dots: false,
+    margin: 30,
+    navText: [
+        "<i class='fas fa-angle-left' aria-hidden='true'></i>",
+        "<i class='fas fa-angle-right' aria-hidden='true'></i>"
+    ],
+})
+
+$('#galeri-slider').owlCarousel({
+    loop: true,
+    nav: true,
+    items: 3,
+    dots: false,
+    margin: 30,
+    navText: [
+        "<i class='fas fa-angle-left' aria-hidden='true'></i>",
+        "<i class='fas fa-angle-right' aria-hidden='true'></i>"
+    ],
+})
